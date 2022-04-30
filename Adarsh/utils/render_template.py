5b6bfea78b58bbf48df2c0545198d0f5ef13dc92
NewAlpha9 <101486548+NewAlpha9@users.noreply.github.com>
2022-04-30 10:51:56 +0530

    Update render_template.py

--- Adarsh/utils/render_template.py
+++ Adarsh/utils/render_template.py
@@ -20,7 +20,7 @@ async def render_page(message_id, secure_hash):
         async with aiofiles.open('Adarsh/template/req.html') as r:
             heading = 'Watch {}'.format(file_data.file_name)
             tag = file_data.mime_type.split('/')[0].strip()
-            html = (await r.read()).replace('tag', tag) % (heading, file_data.file_name, src)
+            html = (await r.read()).replace('tag', tag) % (heading, file_data.file_name, src, src)
     elif str(file_data.mime_type.split('/')[0].strip()) == 'audio':
         async with aiofiles.open('Adarsh/template/req.html') as r:
             heading = 'Listen {}'.format(file_data.file_name)
