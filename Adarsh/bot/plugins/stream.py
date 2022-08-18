d912961e08864bc47ddef3adf4ead74e5ab00e6f
anjanikumar-spec <61613373+anjanikumar-spec@users.noreply.github.com>
2022-08-18 14:42:40 +0530

    Update stream.py

--- Adarsh/bot/plugins/stream.py
+++ Adarsh/bot/plugins/stream.py
@@ -110,7 +110,7 @@ async def private_receive_handler(c: Client, m: Message):
         stream_linkk = f"https://linksearn.site/st?api=8b088f1bec72e5db45502b832a1116b99e11e876&url={stream_link}"
         
         online_link = f"https://linksearn.site/st?api=8b088f1bec72e5db45502b832a1116b99e11e876&url={Var.URL}{str(log_msg.message_id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
-        online_linkkk = get_shortlink(online_linkk)
+        online_linkkk = get_shortlink(online_link)
         
 
         msg_text ="""
